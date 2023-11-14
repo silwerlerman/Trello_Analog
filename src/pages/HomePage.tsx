8f5df4d98ccf372b7f20ca91c3b2ea14f7e50b06
llerman <llerman@nota.tech>
2023-11-14 22:06:55 +0300

    Исправлено использование названий этапов из метаданных

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,8 @@
 import { Stage } from '@components/Stage/Stage';
-import { Stage as StageType } from '@types';
 import { Outlet } from 'react-router-dom';
+import { stages } from 'src/metadata';
 
 export const HomePage = () => {
-  const stages: StageType[] = [
-    {
-      name: 'Назначено'
-    },
-    { name: 'На паузе' },
-    { name: 'В работе' },
-    {
-      name: 'На проверке'
-    },
-    {
-      name: 'Выполнено'
-    }
-  ];
   const stagesToShow = stages.map((stage, i) => (
     <Stage stage={stage} key={i} />
   ));
